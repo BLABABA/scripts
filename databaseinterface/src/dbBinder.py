1dd6060835a6230c0f2e60cb4a8d3d13199fd46d
BLABABA <xingyul1@andrew.cmu.edu>
2019-10-31 13:42:27 -0400

    just push it
    nothing bit deal

--- databaseinterface/src/dbBinder.py
+++ databaseinterface/src/dbBinder.py
@@ -2,7 +2,14 @@
     Author: Xingyu (Alfred) Liu 
     Email: xingyu.alfred.liu@gmail.com
     Description:
-        This class takes care of database interface
+        This class serves as the database interface
+
+        Functions:
+            - read in database with given path
+            data has to be in the JSON format
+            - wirte properties with give key path
+            - list out the key path available
+            - overwrite data (dangerous, backup?)
 """
 
 import os
--- struct/mol2molMatrix.py
+++ struct/mol2molMatrix.py
@@ -1,5 +1,41 @@
-import numpy as np
+"""
+
+This function extract all fragments and 
+compute the rotational matrix from origin
+to the target
+
+Some thoughts:
+    - use the single unique atom as an anchor first
+    - use the smallest number of this specy and its cloest atoms  as anchor
+
+"""
+
+from dbaAutomator.functions import *
+from dbaAutomator.ref import *
+from ase.io import read
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen import Molecule
 import os
-import pymatgen as pmg
+import numpy as np
+import operator
+import collections
 
+# this should be part of the workflow ref
+#atomSet = ('H', 'C', 'N', 'O', 'Si', 'P', 'S')
 
+origin = Molecule.from_file('./data/mol2mol/centermol.xyz')
+target = Molecule.from_file('./data/mol2mol/targetmol.xyz')
+
+transVec = target.center_of_mass - origin.center_of_mass
+
+eleCountDict = dict()
+for ele in list(set(origin.species)):
+    eleCountDict[ele] = origin.species.count(ele)
+print("element: number")
+for key, val in eleCountDict.items():
+    print(key,":",val)
+print(transVec)
+sorted_elecount = sorted(eleCountDict.items(), key=lambda kv:kv[1])
+sorted_dict = collections.OrderedDict(sorted_elecount)
+
+print(sorted_dict)
