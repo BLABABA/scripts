cc4363f197bc027a6797da427a5e712ff180d3ec
Alfred Liu <xingyul1@andrew.cmu.edu>
2019-10-31 14:02:55 +0000

    initialize dbBinder
    TODO:
        - figure out the mol2mol matrix
        - implement database interface

---
+++ databaseinterface/src/dbBinder.py
@@ -0,0 +1,11 @@
+"""
+    Author: Xingyu (Alfred) Liu 
+    Email: xingyu.alfred.liu@gmail.com
+    Description:
+        This class takes care of database interface
+"""
+
+import os
+import json
+
+
